46ea985443876c032de3783f017be10ba389201b
Byron Dunkley <byronleigh80@outlook.com>
2026-06-15 21:45:31 +0100

    byr-10 Add Sentry plugin and debug route

---
+++ plugins/sentry.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const fp = require("fastify-plugin");
+const Sentry = require("@sentry/node");
+
+module.exports = fp(async function (fastify, opts) {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.NODE_ENV || "development",
+    tracesSampleRate: 1.0,
+  });
+
+  fastify.addHook("onError", async (request, reply, error) => {
+    Sentry.captureException(error);
+  });
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,10 @@
-'use strict'
+"use strict";
 
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
-}
+  fastify.get("/", async function (request, reply) {
+    return { root: true };
+  });
+  fastify.get("/debug-sentry", function (fastify, res) {
+    throw new Error("My first Sentry error!");
+  });
+};
