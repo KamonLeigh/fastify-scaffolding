c7c4601c72e49f81d0554a28bf32f85a9ac9f35f
Byron Dunkley <byronleigh80@outlook.com>
2026-06-26 00:54:08 +0100

    BYR-11 Add Fastify Redis integration
    - Add dependency "@fastify/redis"
    - Add plugins/redis.js to register the client with REDIS_URL
    - The plugin uses fastify-plugin to register @fastify/redis

---
+++ plugins/redis.js
@@ -0,0 +1,8 @@
+const fp = require("fastify-plugin");
+const fastifyRedis = require("@fastify/redis");
+
+module.exports = fp(async function (fastify, opts) {
+  fastify.register(fastifyRedis, {
+    url: process.env.REDIS_URL,
+  });
+});
